aff0686da7332ecf31966212b43df4d7f3e6d158
Tomasz Czepiel <tomasz.czepiel@yahoo.co.uk>
2014-01-03 22:11:18 +0000

    - modularised the codebase, so that code for executing each default http method for a resultset is now in separate method. - allow for overriding methods for particular resultsets or globally. Also respect individual implementations for the rest methods in the resultset classes.

--- lib/Plack/App/DBIC.pm
+++ lib/Plack/App/DBIC.pm
@@ -6,8 +6,99 @@ use Data::Dumper;
 use parent 'Plack::Component';
 use Plack::Request;
 use Plack::Response;
-use Plack::Util::Accessor qw(schema serializers);
+use Plack::Util::Accessor qw(schema serializers override);
 use DBIx::Class;
+use Package::Stash;
+use Carp qw(croak);
+
+sub __GET {
+    my ($self,$req,$resultset,$rs,@args) = @_;
+
+    my ($primary) = $rs->result_source->primary_columns();
+
+    my @ret = $rs->search({
+            $primary => { -in => \@args },
+        },
+        {
+            result_class => 'DBIx::Class::ResultClass::HashRefInflator',
+        }
+    );
+
+    my $response = Plack::Response->new();
+    if ( @ret ) {
+        $response->status(200);
+
+        my ($content_type, $data) = $self->serializer($req)->(\@ret);
+
+        $response->content_type($content_type);
+        $response->body( $data );
+    }
+    else {
+        $response->status(404);
+    }
+
+    return $response;
+}
+
+sub __POST {
+    my ($self,$req,$resultset,$rs,@args) = @_;
+    my $ret = $rs->find($args[0]);
+    $ret->update(%{ $req->body_parameters });
+    return $req->new_response(200);
+}
+
+sub __PUT {
+    my ($self,$req,$resultset,$rs,@args) = @_;
+
+    my $res = $rs->create(\%{
+        $req->body_parameters
+    });
+
+    #return primary keys back
+    my @primary = $rs->result_source->primary_columns();
+    my $resp    = $req->new_response(200);
+    my ($content_type,$data) 
+        = $self->serializer($req)->((map { $_ => $res->$_ } @primary));
+
+    $resp->content_type($content_type);
+    $resp->body($data);
+    return $resp;
+}
+
+sub __DELETE {
+    my ($self,$req,$resultset,$rs,@args) = @_;
+
+    my ($primary) = $rs->result_source->primary_columns();
+    $rs->search({
+        $primary => { -in => \@args },
+    })->delete();
+
+    return $req->new_response(200);
+}
+
+our $rest2subref;
+sub _rest2subref {
+    my ($self,$req,$resultset) = @_;
+    my $method = $req->method();
+
+    my $subref = $rest2subref->{$resultset}{$method} ||= do {
+        my $rs = $self->schema->resultset($resultset);
+        if ( my $coderef = $rs->can($method) ) {
+            return $coderef;
+        }
+        elsif ( my $overrides = $self->override ) {
+            croak "override parameter must be a hashref"
+                unless ref $overrides eq 'HASH';
+            my $coderef = $overrides->{$resultset}{$method} || $overrides->{$method};
+            return $coderef if $coderef;
+        }
+
+        my $p = Package::Stash->new(__PACKAGE__);
+        return $p->get_symbol('&__'.$method);
+    };
+
+    return $subref;
+}
 
 sub serializer {
     my $self    = shift;
@@ -36,69 +127,11 @@ sub call {
     my @args = ($args =~ /,/ ? (split ",", $args) : $args);
 
     if ( my $rs = $self->schema->resultset($resultset) ) {
-        my ($primary) = $rs->result_source->primary_columns();
 
-        if ( $req->method eq 'GET' ) {
+        my $dispatch_method = $self->_rest2subref($req,$resultset);
+        my $response        = $dispatch_method->($self,$req,$resultset,$rs,@args);
 
-            my @ret = $rs->search({
-                    $primary => { -in => \@args },
-                },
-                {
-                    result_class => 'DBIx::Class::ResultClass::HashRefInflator',
-                }
-            );
-
-            my $response = Plack::Response->new();
-            if ( @ret ) {
-
-                $response->status(200);
-
-                my ($content_type, $data) = $self->serializer($req)->(\@ret);
-
-                $response->content_type($content_type);
-                $response->body( $data );
-
-                return $response->finalize;
-            }
-
-            $response->status(404);
-
-            return $response->finalize;
-            
-        }
-        elsif ( $req->method eq 'POST')  {
-           my $ret = $rs->find($args[0]);
-
-          $ret->update(%{ $req->body_parameters });
-
-          my $res = $req->new_response(200);
-          return $res->finalize;
-
-        }
-        elsif ( $req->method eq 'PUT' ) {
-
-            my $res = $rs->create(\%{
-                $req->body_parameters
-            });
-            my @primary = $rs->result_source->primary_columns();
-
-            my $resp = $req->new_response(200);
-
-            my ($content_type,$data) = $self->serializer($req)->((map { $_ => $res->$_ } @primary));
-            $resp->content_type($content_type);
-            $resp->body($data);
-
-            return $resp->finalize();
-        }
-        elsif ( $req->method eq 'DELETE' ) {
-
-            $rs->search({
-                $primary => { -in => \@args },
-            })->delete();
-
-            my $res = $req->new_response(200);
-            return $res->finalize;
-        }
+        return $response->finalize;
     }
     else {
         my $resp = Plack::Response->new(404);
