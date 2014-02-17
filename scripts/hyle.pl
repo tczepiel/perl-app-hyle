a04c7fa53808157fc51432ca5978f09fffb65318
Tomasz Czepiel <tomasz.czepiel@yahoo.co.uk>
2014-02-17 17:26:34 +0000

    more tests.

--- scripts/hyle.pl
+++ scripts/hyle.pl
@@ -12,63 +12,67 @@ use Getopt::Long;
 use Plack::Runner;
 use Hyle;
 
-my ($dsn,$username,$password);
-GetOptions("dsn=s" => \$dsn, "user=s" => \$username, "pass=s" => \$password)
-    or die "can't get the options";
+sub run {
+    my ($dsn,$username,$password);
+    GetOptions("dsn=s" => \$dsn, "user=s" => \$username, "pass=s" => \$password)
+        or die "can't get the options";
 
 
-unless ($dsn) {
-    print STDERR <<"EOF";
+    unless ($dsn) {
+    print <<"EOF";
     Usage: hyle.pl --dsn "dbi:SQLite:dbname=dbfile"
 EOF
 
-    exit 1;
-}
+        exit 1;
+    }
 
-my ($prog) = $password ? ($0 =~ s/$password/xxxxxxxx/g) : undef;
-$0 = $prog if $prog;
+    my ($prog) = $password ? ($0 =~ s/$password/xxxxxxxx/g) : undef;
+    $0 = $prog if $prog;
 
-$password ||= '';
-$username ||= '';
+    $password ||= '';
+    $username ||= '';
 
-my $tempdir = tempdir(CLEANUP => 1);
-DBIx::Class::Schema::Loader::make_schema_at(
-    'Schema',
-    { dump_directory => $tempdir },
-    [ $dsn,  $username,$password, {} ],
-);
+    my $tempdir = tempdir(CLEANUP => 1);
+    DBIx::Class::Schema::Loader::make_schema_at(
+        'Schema',
+        { dump_directory => $tempdir },
+        [ $dsn,  $username,$password, {} ],
+    );
 
 
-push @INC, $tempdir;
+    push @INC, $tempdir;
 
-eval {
-    require Schema;
-    Schema->import();
-    1;
-} or do {
-    my $err = $@ || "unknown";
-    die $err;
-};
+    eval {
+        require Schema;
+        Schema->import();
+        1;
+    } or do {
+        my $err = $@ || "unknown";
+        die $err;
+    };
 
 # get the scema
-my $schema = Schema->connect($dsn, $username, $password);
+    my $schema = Schema->connect($dsn, $username, $password);
 
-for my $source ($schema->sources() ) {
-    # hack, add something as a primary column if no primary column(s) are defined
-    my $source_class = $schema->source($source);
-    next if $source_class->primary_columns();
+    for my $source ($schema->sources() ) {
+        # hack, add something as a primary column if no primary column(s) are defined
+        my $source_class = $schema->source($source);
+        next if $source_class->primary_columns();
 
-    print STDERR <<"EOF";
-    WARNING: Table $source has no primary key defined, defaulting to using all columns.
+        print STDERR <<"EOF";
+        WARNING: Table $source has no primary key defined, defaulting to using all columns.
 EOF
-    $source_class->set_primary_key($source_class->columns());
+        $source_class->set_primary_key($source_class->columns());
+    }
+
+    my $app    = Hyle->new( schema => $schema )->to_app;
+    my $runner = Plack::Runner->new();
+    $runner->parse_options(@ARGV);
+
+    $runner->run($app);
 }
 
-my $app    = Hyle->new( schema => $schema )->to_app;
-my $runner = Plack::Runner->new();
-$runner->parse_options(@ARGV);
-
-$runner->run($app);
+run() unless caller();
 
 __END__
 
